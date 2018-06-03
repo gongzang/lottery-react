636d6606e272a777b65372c0ff53eea74ec8ef9e
ming.zhang <52726643@qq.com>
2018-06-03 21:27:55 +0800

    增加查找dom css属性的工具方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -7,3 +7,7 @@ export function isParent(obj, parentObj) {
     }
     return false;
 } 
+
+export function getDefaultStyle(obj,attribute){ // 返回最终样式函数，兼容IE和DOM，设置参数：元素对象、样式特性   
+    return obj.currentStyle ? obj.currentStyle[attribute] : document.defaultView.getComputedStyle(obj, false)[attribute];
+}
