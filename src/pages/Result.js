72e29eef2cacdb21507e8579964c60e4f1a6f4d2
ming.zhang <52726643@qq.com>
2018-06-04 21:56:59 +0800

    继续完善彩票开奖信息的布局

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,23 +3,37 @@ import React from 'react';
 import '../styles/result.scss';
 import Headline from '../components/headline/Headline';
 import Nix from '../components/nix/Nix';
+import { get } from '../utils/request';
 
 
 class Result extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            lotteryTitle: 'resultTitle',
             ballResult: [[0, 2], [0, 6], [1, 3], [2, 2], [3, 1]],
             wrapperWords: ['789', '987', 'abc', 'def']
         };
     }
+    componentWillMount() {
+        var shelf = this;
+        get(`/lottery/queryNewest?lottery_id=${'ssq'}`)
+            .then((res) => {
+                this.setState({
+                    lotteryTitle: res.lottery_name||'',
+                    ballResult: res.lotteryResArr||[],
+                    wrapperWords: res.lotteryMessage||[]
+                });
+            });
+    }
 
     render() {
-        const {ballResult,wrapperWords} = this.state;
+        const { ballResult, wrapperWords, lotteryTitle } = this.state;
         return (
             <div className='ResultDiv'>
+                <h1 className='resultTitle'>{lotteryTitle}</h1>
                 <Nix ballResult={ballResult} />
-                <Headline staticWords={'123456'} options={{ hasClip: true }} wrapperWords={wrapperWords} />
+                <Headline staticWords='附加信息  ' options={{ hasClip: true }} wrapperWords={wrapperWords} />
             </div>
         );
     }
