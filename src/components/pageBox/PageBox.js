6946f1e52aefaacafc148444be1af0d2d82eb033
ming.zhang <52726643@qq.com>
2018-06-06 21:00:50 +0800

    完善分页的样式

--- src/components/pageBox/PageBox.js
+++ src/components/pageBox/PageBox.js
@@ -24,8 +24,8 @@ class PageBox extends React.Component {
                                             <span aria-hidden="true">&laquo;</span>
                                         </a>
                                     </li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">1234</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2567</a></li>
                                     <li class="page-item active"><a class="page-link" href="#">3</a></li>
                                     <li class="page-item"><a class="page-link" href="#">4</a></li>
                                     <li class="page-item"><a class="page-link" href="#">5</a></li>
